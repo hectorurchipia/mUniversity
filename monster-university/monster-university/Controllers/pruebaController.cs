1bc113ea9bbd47e93fb7238e33988ebcbfe677b0
Hector Urchipia <89946620+hectorurchipia@users.noreply.github.com>
2022-07-08 15:22:32 -0300

    cree controller a modo prueba para get

---
+++ monster-university/monster-university/Controllers/pruebaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace monster_university.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Hola(string para)
+        {
+            return View();
+        }
+
+    }
+}
